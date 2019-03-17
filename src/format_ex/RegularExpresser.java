c647a25656e8af09c2977ee8d375784f8aa7de07
redhood <2043570808@qq.com>
2019-03-17 20:37:06 +0800

    #@

---
+++ src/format_ex/RegularExpresser.java
@@ -0,0 +1,10 @@
+package format_ex;
+
+public class RegularExpresser {
+    public static void main(String[] args) {
+        System.out.println("-1234".matches("-?\\d+"));
+        System.out.println("5678".matches("-?\\d+"));
+        System.out.println("+911".matches("-?\\d+"));
+        System.out.println("+911".matches("(-||\\+)?\\d+"));
+    }
+}
